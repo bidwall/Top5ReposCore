91b1c4ae6eacd790b55f6f18be3eb71acb1cc6fc
Palvinder Bidwall <pal@bidwall.co.uk>
2017-02-24 15:36:24 +0000

    Added HttpClientHelperCore.Tests
    Tests not runnable as runners do not support the new csproj files

---
+++ src/HttpClientHelperCore.Tests/GitHubHttpClientHelperTests.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Linq.Expressions;
+using Moq;
+using NUnit.Framework;
+
+namespace HttpClientHelpers.Test
+{
+    [TestFixture]
+    public class GitHubHttpClientHelperTests
+    {
+        private Mock<IHttpResponseProvider> _mockHttpReponseProvider;
+        private GitHubHttpClientHelper _gitHubHttpClientHelper;
+        private const string Url = "http://github.com/";
+
+        [SetUp]
+        public void TestSetup()
+        {
+            _mockHttpReponseProvider = new Mock<IHttpResponseProvider>();
+            _gitHubHttpClientHelper = new GitHubHttpClientHelper(_mockHttpReponseProvider.Object);
+        }
+
+        [Test]
+        public void GetDataFromUrl_GiveUrl_SetsBaseAddress()
+        {
+            //Act
+            _gitHubHttpClientHelper.GetDataFromUrl<string>(Url);
+
+            //Assert
+            Expression<Func<HttpClientConfig, bool>> expression = y => y.BaseAddress.AbsoluteUri == Url;
+            _mockHttpReponseProvider.Verify(x => x.GetResponse<string>(It.Is(expression)), Times.Once);
+        }
+
+        [Test]
+        public void GetDataFromUrl_SetsRequestUriToEmptyString()
+        {
+            //Act
+            _gitHubHttpClientHelper.GetDataFromUrl<string>(Url);
+
+            //Assert
+            Expression<Func<HttpClientConfig, bool>> expression = y => y.RequestUri == string.Empty;
+            _mockHttpReponseProvider.Verify(x => x.GetResponse<string>(It.Is(expression)), Times.Once);
+        }
+
+        [Test]
+        public void GetDataFromUrl_SetsAcceptsHeaders()
+        {
+            //Act
+            _gitHubHttpClientHelper.GetDataFromUrl<string>(Url);
+
+            //Assert
+            Expression<Func<HttpClientConfig, bool>> expression = y => y.AcceptHeaders.Count == 1 && y.AcceptHeaders[0].MediaType == Constants.JsonContentType;
+            _mockHttpReponseProvider.Verify(x => x.GetResponse<string>(It.Is(expression)), Times.Once);
+        }
+
+        [Test]
+        public void GetDataFromUrl_SetsUserAgentHeaders()
+        {
+            //Act
+            _gitHubHttpClientHelper.GetDataFromUrl<string>(Url);
+
+            //Assert
+            Expression<Func<HttpClientConfig, bool>> expression = y => y.UserAgentHeaders.Count == 1 &&
+                                                                       y.UserAgentHeaders[0].Key == Constants.UserAgentHeaderKey &&
+                                                                       y.UserAgentHeaders[0].Value == string.Empty;
+
+            _mockHttpReponseProvider.Verify(x => x.GetResponse<string>(It.Is(expression)), Times.Once);
+        }
+    }
+}
